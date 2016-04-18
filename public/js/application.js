4164a15c26b353d2c9ec3e2210a0b37bcfb91662
gabrielmedina <gmedina.santos@gmail.com>
2016-04-18 14:33:55 -0400

    Adicionando JS processado

---
+++ public/js/application.js
@@ -0,0 +1,1 @@
+!function(){var link=document.querySelector(".video__link--youtube"),image=document.querySelector(".video__image");link.addEventListener("click",function(e){e.preventDefault(),this.innerHTML='<iframe width="'+image.offsetWidth+'" height="'+image.offsetHeight+'" src="'+this.href+'?autoplay=1;rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>'})}();
